dc6ec412346856e37eb98786f8791a299832ea8a
NickanTaheri <55196928+NickanTaheri@users.noreply.github.com>
2022-10-24 20:47:29 +0800

    map updated

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
 let myMap = L.map("map", {
-    center: [-26, 122],
-    zoom: 7
+    center: [-24, 120],
+    zoom: 5
   });
   
   // Adding the tile layer
@@ -9,28 +9,28 @@ let myMap = L.map("map", {
     //   tileSize: 500
   }).addTo(myMap);
 
-  // let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/Water_Hydrant_WCORP_070_WA_GDA2020_Public.geojson";
+  let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/Water_Hydrant_WCORP_070_WA_GDA2020_Public.geojson";
   
-  // d3.json(url).then(function(response) {
+  d3.json(url).then(function(response) {
   
-  //   console.log(response);
-  //   features = response.features;
+    console.log(response);
+    features = response.features;
   
-  //   let heatArray = [];
+    let heatArray = [];
   
-  //   for (let i = 0; i < features.length; i++) {
-  //     let location = features[i].geometry;
-  //     if (location) {
-  //       //console.log(location);
-  //       heatArray.push([location.coordinates[1], location.coordinates[0]]);
-  //     }
+    for (let i = 0; i < features.length; i++) {
+      let location = features[i].geometry;
+      if (location) {
+        //console.log(location);
+        heatArray.push([location.coordinates[1], location.coordinates[0]]);
+      }
   
-  //   }
+    }
   
-  //   let heat = L.heatLayer(heatArray, {
-  //     radius: 20,
-  //     blur: 35
-  //   }).addTo(myMap);
+    let heat = L.heatLayer(heatArray, {
+      radius: 20,
+      blur: 35
+    }).addTo(myMap);
   
-  // });
+  });
   
