4440db24d29ced0676afbe6f746dc5221d1660aa
Danny <dannybruzz@hotmail.com>
2022-10-26 22:54:09 +0800

    sidebar in progress

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -43,3 +43,64 @@ function buildBarPlot() {
   });}
   buildBarPlot()
 
+  function read() {
+    d3.json("/api/dashboard/commodities").then(function (response) {
+      console.log(response)
+  })}
+
+  // function commoditybar(commodity) {
+  //   let PANEL = d3.select("#sample-metadata");
+  //   PANEL.html("");
+  //   d3.json("/api/dashboard/commodities").then(function (response) {
+  //     let searchTerm = [];
+  //     if (commodity === "copper") {
+  //       searchTerm.push("Copper")
+  //     }
+  //     else if (commodity === "gold") {
+  //       searchTerm.push("Gold")
+  //     }
+  //     else if (commodity === "iron_ore") {
+  //       searchTerm.push("Iron Ore")
+  //     }
+  //     else if (commodity === "lead") {
+  //       searchTerm.push("Lead")
+  //     }
+  //     else if (commodity === "lithium") {
+  //       searchTerm.push("Lithium")
+  //     }
+  //     else if (commodity === "manganese") {
+  //       searchTerm.push("Manganese")
+  //     }
+  //     else if (commodity === "nickel") {
+  //       searchTerm.push("Nickel")
+  //     }
+  //     else if (commodity === "silver") {
+  //       searchTerm.push("Silver")
+  //     }
+  //     else if (commodity === "tin") {
+  //       searchTerm.push("Tin")
+  //     }
+  //     else if (commodity === "zinc") {
+  //       searchTerm.push("Zinc")
+  //     }
+  //     console.log(searchTerm);
+  
+  //     for (i = 0; i < response.length; i++){
+  //       row = response[i];
+  //       if (row[1] === searchTerm){
+  //         PANEL.append("h6").text(`Commodity: ${row[1]}`);
+  //         console.log(row[1]);
+  //         PANEL.append("h6").text(`Current price: ${row[2]}`);
+  //         console.log(row[2]);
+  //         PANEL.append("h6").text(`Q4/22: $${row[3]}`);
+  //         console.log(row[3]);
+  //         PANEL.append("h6").text(`Q1/23: $${row[4]}`);
+  //         console.log(row[4]);
+  //         PANEL.append("h6").text(`Q2/23: $${row[5]}`);
+  //         console.log(row[5]);
+  //         PANEL.append("h6").text(`Q/23: $${row[6]}`);
+  //         console.log(row[6]);
+  //       }}
+  //   })};
+    
+    
